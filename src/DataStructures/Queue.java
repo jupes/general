9a76f6808acb2466a3a8d2a5a2b1f03e9db1267d
Erich <erich.staehling@gmail.com>
2019-12-17 12:13:49 -0500

    add queue implemented in two stack, one with old data and one with new data

--- src/DataStructures/Queue.java
+++ src/DataStructures/Queue.java
@@ -11,5 +11,48 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Queue {
-    
+    public static class MyQueue<T> {
+        private Stack<T> newDataStack = new Stack<T>();
+        private Stack<T> oldDataStack = new Stack<T>();
+
+        /**
+         * Add item to queue
+         * 
+         * @param value item to be added
+         */
+        public void enqueue(T value) {
+            newDataStack.push(value);
+        }
+
+        /**
+         * Get oldest item from queue
+         * 
+         * @return oldest item
+         */
+        public T peek() {
+            shiftStacks();
+            return oldDataStack.peek();
+        }
+        
+        /**
+         * Move the new data into the old data if the old data is empty
+         */
+        public void shiftStacks() {
+            if (oldDataStack.isEmpty()) {
+                while(!newDataStack.isEmpty()) {
+                    oldDataStack.push(newDataStack.pop());
+                }
+            }
+        }
+
+        /**
+         * remove oldest item from queue
+         * 
+         * @return oldest item
+         */
+        public T dequeue() {
+            shiftStacks();
+            return oldDataStack.pop();
+        }
+    }
 }
