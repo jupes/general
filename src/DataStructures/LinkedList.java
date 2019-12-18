5c56976ee0b4e083406478244362221709e98d83
Erich <erich.staehling@gmail.com>
2019-12-18 11:11:45 -0500

    add linked list implementation

--- src/DataStructures/LinkedList.java
+++ src/DataStructures/LinkedList.java
@@ -8,20 +8,48 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-/**
- * Implement Stack using a linked list, the code does not handle null pointers as is
- */
-public static class LinkedList {
-    private static class Node {
-        private int data;
-        private Node next;
+public class Node {
+    int data;
+    Node next;
 
-        private Node(int data) {
-            this.data = data;
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
+public class LinkedList {
+    Node head;
+
+    public void append(int data) {
+        if (head == null) {
+            head = new Node(data);
         }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = new Node(data);
     }
 
-    private Node top; // always access top first
+    public void prepend(int data) {
+        Node newHead = new Node(data);
+        newHead.next = head;
+        head = newHead;
+    }
 
-    
+    public void deleteWithValue(int data) {
+        if (head == null)
+            return;
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                return;
+            }
+        }
+    }
 }
