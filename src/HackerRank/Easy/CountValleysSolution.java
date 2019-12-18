2855a38b05035393d678cc9f39286c054d9e48c7
Erich <erich.staehling@gmail.com>
2019-12-18 12:12:29 -0500

    format files

--- src/HackerRank/Easy/CountValleysSolution.java
+++ src/HackerRank/Easy/CountValleysSolution.java
@@ -13,8 +13,11 @@ public class CountValleysSolution {
 
     // Complete the countingValleys function below.
     /**
-     * This solution takes advantage of the idea that any time you end up at sea level and the last check was uphill you had to have been in a valley.
-     * So for each time you altitude (var alt) reaches zero from going uphill, add a valley to the count
+     * This solution takes advantage of the idea that any time you end up at sea
+     * level and the last check was uphill you had to have been in a valley. So for
+     * each time you altitude (var alt) reaches zero from going uphill, add a valley
+     * to the count
+     * 
      * @param n length of the string
      * @param s represents the steps uphill and downhill
      * @return number of valleys encountered in hiking
@@ -43,7 +46,6 @@ public class CountValleysSolution {
         String testStr1 = "DU";
         result = countingValleys(testStr1.length(), testStr1);
         System.out.println("Expected Result: 1, Actual Result: " + result);
-
 
         // Test Larger String
         String testStr2 = "UDDDUDUU";
--- src/HackerRank/Easy/JumpingOnCloudsSolution.java
+++ src/HackerRank/Easy/JumpingOnCloudsSolution.java
@@ -14,32 +14,28 @@ public class JumpingOnCloudsSolution {
     static int jumpingOnClouds(int[] c) {
         int result = 0;
         int i = 0;
-        while(i < c.length-1) {
-            if(c.length > i+2 && c[i+2] == 1) {
+        while (i < c.length - 1) {
+            if (c.length > i + 2 && c[i + 2] == 1) {
                 i++;
-                
             } else {
-                i+=2;
-                
+                i += 2;
             }
             result++;
         }
         return result;
     }
 
-
     public static void main(String[] args) throws IOException {
-      
+
         int result = 0;
 
-        // Test Unsorted Array 
-        int[] testArray1 = {0, 0, 1, 0, 0, 1, 0};
+        // Test Unsorted Array
+        int[] testArray1 = { 0, 0, 1, 0, 0, 1, 0 };
         result = jumpingOnClouds(testArray1);
         System.out.println("Expected Result: 4, Actual Result: " + result);
 
-
-        // Test Larger Unsorted Array 
-        int[] testArray2 = {0,0,0,0,1,0};
+        // Test Larger Unsorted Array
+        int[] testArray2 = { 0, 0, 0, 0, 1, 0 };
         result = jumpingOnClouds(testArray2);
         System.out.println("Expected Result: 3, Actual Result: " + result);
 
