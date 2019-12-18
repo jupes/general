c6037b7a3d194613853d0f6936a1b777ec1fd287
Erich <erich.staehling@gmail.com>
2019-12-18 11:29:35 -0500

    add cycle detection function to linked list

--- src/DataStructures/LinkedList.java
+++ src/DataStructures/LinkedList.java
@@ -52,4 +52,23 @@ public class LinkedList {
             }
         }
     }
+    /**
+     * Method returns if there is a cycle in the linked list, think of cars going around a race track, eventually they will collide if there is a cycle
+     * @param head start of linked list
+     * @return
+     */
+    boolean hasCycle(Node head) {
+        if (head == null)
+            return false;
+        Node fast = head.next;
+        Node slow = head;
+        while (fast != null && fast.next != null && slow != null) {
+            if (fast == slow) {
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
 }
