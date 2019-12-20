be7f3da24b9e44bbbe4ff2f9752ec0aa8ed5f726
Erich <erich.staehling@gmail.com>
2019-12-20 10:57:00 -0500

    format document

--- src/Algorithms/IceCreamParlorProblem.java
+++ src/Algorithms/IceCreamParlorProblem.java
@@ -11,7 +11,7 @@ import java.util.regex.*;
 // Point of this one is to, starting with some amount of money, find a pair of unique flavors that sum to equal my total cash
 public class IceCreamParlorProblem {
     public static int indexOf(int[] menu, int index, int excludeThis) {
-        for(int i = 0; i < menu.length;i++){
+        for (int i = 0; i < menu.length; i++) {
             if (menu[i] == value && i != excludeThis) {
                 return i;
             }
@@ -21,7 +21,7 @@ public class IceCreamParlorProblem {
     public static int[] getIndiciesFromValues(int[] menu, int value1, int value2) {
         int index1 = indexOf(menu, value1, -1);
         int index2 = indexOf(menu, value2, index1);
-        int[] indicies = {Math.min(index1, index2), Math.max(index1, index2)};
+        int[] indicies = { Math.min(index1, index2), Math.max(index1, index2) };
         return indicies;
     }
 
@@ -31,12 +31,12 @@ public class IceCreamParlorProblem {
 
         for (int i = 0; i < sortedMenu.length; i++) {
             int complement = money - sortedMenu[i];
-            int location = Arrays.binarySearch(sortedMenu, i+1, sortedMenu.length, complement);
+            int location = Arrays.binarySearch(sortedMenu, i + 1, sortedMenu.length, complement);
             if (location >= 0 && location < menu.length && sortedMenu[location] == complement) {
                 int[] indicies = getIndiciesFromValues(menu, sortedMenu[i], compliment);
                 return indicies;
-            } 
+            }
         }
-        return indicies;
+        return null;
     }
 }
